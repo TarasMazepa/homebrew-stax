234faab9e1516e5c677ae22e1ce453cc30db285a
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2024-06-13 23:04:42 -0700

    Update stax.rb

--- Formula/stax.rb
+++ Formula/stax.rb
@@ -5,9 +5,9 @@ class Stax < Formula
       using: :git,
       branch: "main"
   version "0.6.2"
-  head "https://github.com/TarasMazepa/stax.git", 
-      using: :git
-  
+  head "https://github.com/TarasMazepa/stax.git",
+       using: :git
+
   depends_on "dart-lang/dart/dart" => :build
 
   def install

d27bb174e385c4f1f1766f9448949594c64eabd5
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2024-07-07 21:32:40 -0700

    Update stax.rb

--- Formula/stax.rb
+++ Formula/stax.rb
@@ -4,7 +4,7 @@ class Stax < Formula
   url "https://github.com/TarasMazepa/stax.git",
       using:  :git,
       branch: "main"
-  version "0.6.10"
+  version "0.7.0"
   head "https://github.com/TarasMazepa/stax.git", using: :git
 
   depends_on "dart-lang/dart/dart" => :build

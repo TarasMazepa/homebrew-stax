2f8fce02aa39d1429f870b36b516714207b08b06
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2024-07-15 20:05:03 -0700

    Update stax.rb

--- Formula/stax.rb
+++ Formula/stax.rb
@@ -1,9 +1,7 @@
 class Stax < Formula
   desc "Wrapper around git, which helps with stax-like git workflow"
   homepage "https://staxforgit.com/"
-  tag="0.7.4"
-  url "https://github.com/TarasMazepa/stax.git", tag: tag.to_s
-  version tag.presence || "0.0.1"
+  url "https://github.com/TarasMazepa/stax.git", tag: "0.7.4"
 
   depends_on "dart-lang/dart/dart" => :build
 

2881b66f39756fd3b115d52ccb8fe18b10c75e05
stax <stax@staxforgit.com>
2024-07-19 21:57:23 +0000

    Update stax to 0.7.32

--- Formula/stax.rb
+++ Formula/stax.rb
@@ -1,7 +1,7 @@
 class Stax < Formula
   desc "Wrapper around git, which helps with stax-like git workflow"
   homepage "https://staxforgit.com/"
-  url "https://github.com/TarasMazepa/stax.git", tag: '0.7.31'
+  url "https://github.com/TarasMazepa/stax.git", tag: '0.7.32'
 
   depends_on "dart-lang/dart/dart" => :build
 

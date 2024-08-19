1d4445eb5ad80433fdb1294afca768a1400ae68c
stax <stax@staxforgit.com>
2024-08-19 03:46:50 +0000

    Update stax to 0.9.17

--- Formula/stax.rb
+++ Formula/stax.rb
@@ -3,11 +3,11 @@ class Stax < Formula
   homepage "https://staxforgit.com/"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/TarasMazepa/stax/releases/download/0.9.16/macos-x64.zip"
+    url "https://github.com/TarasMazepa/stax/releases/download/0.9.17/macos-x64.zip"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/TarasMazepa/stax/releases/download/0.9.16/macos-arm.zip"
+    url "https://github.com/TarasMazepa/stax/releases/download/0.9.17/macos-arm.zip"
   elsif OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/TarasMazepa/stax/releases/download/0.9.16/linux-x64.zip"
+    url "https://github.com/TarasMazepa/stax/releases/download/0.9.17/linux-x64.zip"
   else
     depends_on 'dart-lang/dart/dart@3.5.1' => :build
     url "https://github.com/TarasMazepa/stax.git", tag: '0.9.17'

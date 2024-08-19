aede4a229320c0692ad736fbce1c8dc160fa49f8
stax <stax@staxforgit.com>
2024-08-18 17:10:49 -0700

    Delivering prebuilds for Mac arm,x64 and Linux x64

--- Formula/stax.rb
+++ Formula/stax.rb
@@ -1,14 +1,26 @@
 class Stax < Formula
   desc "Stack your PRs with stax"
   homepage "https://staxforgit.com/"
-  url "https://github.com/TarasMazepa/stax.git", tag: '0.9.16'
 
-  depends_on 'dart-lang/dart/dart@3.5.1' => :build
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/TarasMazepa/stax/releases/download/0.9.16/macos-x64.zip"
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/TarasMazepa/stax/releases/download/0.9.16/macos-arm.zip"
+  elsif OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+    url "https://github.com/TarasMazepa/stax/releases/download/0.9.16/linux-x64.zip"
+  else
+    depends_on 'dart-lang/dart/dart@3.5.1' => :build
+    url "https://github.com/TarasMazepa/stax.git", tag: '0.9.16'
+  end
 
   def install
-    cd "cli" do
-      system "dart", "pub", "get"
-      system "dart", "compile", "exe", "bin/cli.dart", "-o", "stax", "-Dversion=#{version}"
+    if Dir.exist?("cli")
+      cd "cli" do
+        system "dart", "pub", "get"
+        system "dart", "compile", "exe", "bin/cli.dart", "-o", "stax", "-Dversion=#{version}"
+        bin.install "stax"
+      end
+    else
       bin.install "stax"
     end
   end

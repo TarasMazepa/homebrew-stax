184b6f2b5baf0eec9d0b02f2edc82ed25957654d
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2025-01-02 21:22:25 -0800

    Update stax to 0.9.32 (#87)
    Co-authored-by: stax <stax@staxforgit.com>

--- Formula/stax.rb
+++ Formula/stax.rb
@@ -1,20 +1,20 @@
 class Stax < Formula
   desc "Stack your PRs with stax"
   homepage "https://staxforgit.com/"
-  version '0.9.31'
+  version '0.9.32'
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/TarasMazepa/stax/releases/download/0.9.31/macos-x64.zip"
-    sha256 '6d8e0c0968ab6463ce1244864cae62fd22d4caa6600319547ddb0fef69a7d6c8' # macos-x64
+    url "https://github.com/TarasMazepa/stax/releases/download/0.9.32/macos-x64.zip"
+    sha256 '19262ebeccc8ec520ee3b1483eca1955e120cee9c2921c45a4591116ef459a59' # macos-x64
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/TarasMazepa/stax/releases/download/0.9.31/macos-arm.zip"
-    sha256 'd4c19acb24f55f3f80d7d67c5e5eb2719d26c9fb388702acc244840f14598d18' # macos-arm
+    url "https://github.com/TarasMazepa/stax/releases/download/0.9.32/macos-arm.zip"
+    sha256 '9dc3f8e20199d55c6728afd3c7bd543f485a3e3c56f839665e0fe954ad9e4f71' # macos-arm
   elsif OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/TarasMazepa/stax/releases/download/0.9.31/linux-x64.zip"
-    sha256 '4761ff7b4fb6db7778f32c8081e7763d7aab2e75aaec1cd1a2b930ae991fb7f6' # linux-x64
+    url "https://github.com/TarasMazepa/stax/releases/download/0.9.32/linux-x64.zip"
+    sha256 '25ffd12f68f0a4afa2bf7db2d3da3003e33cd3d4d23efa83792606440213a430' # linux-x64
   else
     depends_on 'dart-lang/dart/dart@3.5.3' => :build
-    url "https://github.com/TarasMazepa/stax.git", tag: '0.9.31'
+    url "https://github.com/TarasMazepa/stax.git", tag: '0.9.32'
   end
 
   def install
